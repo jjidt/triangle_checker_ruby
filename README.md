7d632891dc74a6db76b382e7c78a29e21ebe007e
Moof Mayeda & J.J. Idt <student@epicodus.com>
2014-07-25 12:30:17 -0700

    slow down functionality of program relative to triangle size

---
+++ README.md
@@ -0,0 +1,6 @@
+# TRIANGLE CHECKER!
+
+triangle sides can't be computed by humans.
+use this program to have your personal computer calculate that thing.
+
+#J.J.Idt && Moof Mayeda
--- triangle_checker.rb
+++ triangle_checker.rb
@@ -8,6 +8,21 @@ sideb = gets.chomp.to_f
 puts "Enter the length of the third side"
 sidec = gets.chomp.to_f
 your_Triangle = Triangle.new(sidea, sideb, sidec)
-puts "That's not a real triangle. Stop playin and get a triangle that actually exists!" if !your_Triangle.is_triangle?
-puts "You've got a #{your_Triangle.type} triangle on your hands!" if your_Triangle.is_triangle?
+
+def dots(num)
+  num.to_i.times do
+  print "."
+  sleep(0.3)
+  end
+end
+
+print "Working"
+dots(sidea)
+print "this is a really tough one"
+dots(sideb)
+print "Analyzing"
+dots(sidec)
+
+puts "\nThat's not a real triangle. Stop playin and get a triangle that actually exists!" if !your_Triangle.is_triangle?
+puts "\nYou've got a #{your_Triangle.type} triangle on your hands!" if your_Triangle.is_triangle?
 exit
